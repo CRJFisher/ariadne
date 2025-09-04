b2a477299f7efa906c9a68380ebec5c42ed7854d
crjfisher <crjfisher@gmail.com>
2025-09-04 14:50:03 +0100

    chore: remove deprecated src_old directory and outdated docs
    - Remove entire src_old directory containing deprecated implementation
    - Remove outdated backlog reorganization documents
    - Remove completed TYPE_HARMONIZATION_PLAN
    - Clean up project structure for epic-11 restructuring

--- packages/core/src_old/project/index.ts
+++
@@ -1,7 +0,0 @@
-// Export project-related modules
-export { Project } from './project';
-export { FileManager } from './file_manager';
-export { LanguageManager } from './language_manager';
-export { CallGraphService } from './call_graph_service';
-export { InheritanceService } from './inheritance_service';
-export { ImportResolver } from './import_resolver';
