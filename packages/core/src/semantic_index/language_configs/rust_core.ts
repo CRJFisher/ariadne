f6d6d6796a515fb23f1279cda537d076ba1ad098
crjfisher <crjfisher@gmail.com>
2025-09-24 11:43:37 +0100

    feat: Add Rust macro capture mappings and enhanced semantic index patterns
    - Add macro capture mappings to rust_core.ts:
      * def.macro -> SemanticEntity.MACRO for macro definitions
      * ref.macro, ref.macro.scoped -> SemanticEntity.MACRO for references
      * ref.macro.builtin with is_builtin modifier for built-in macros
      * ref.macro.async for async macro support
    
    - Enhance Rust semantic patterns with additional entity mappings:
      * Generic struct/enum/function definitions with is_generic modifier
      * Async function definitions with is_async modifier
      * Const/unsafe function modifiers
      * Associated methods and trait implementations
      * Enhanced parameter and variable definitions
      * Module, interface, and type alias improvements
    
    Completes macro system implementation for comprehensive Rust analysis.

--- packages/core/src/semantic_index/language_configs/rust_core.ts
+++ packages/core/src/semantic_index/language_configs/rust_core.ts
@@ -87,10 +87,33 @@ export const RUST_CORE_MAPPINGS = new Map<string, CaptureMapping>([
     },
   ],
   [
+    "def.struct.generic",
+    {
+      category: SemanticCategory.DEFINITION,
+      entity: SemanticEntity.CLASS,
+      modifiers: () => ({ is_generic: true }),
+    },
+  ],
+  [
     "def.enum",
     {
       category: SemanticCategory.DEFINITION,
       entity: SemanticEntity.ENUM,
+    },
+  ],
+  [
+    "def.enum.generic",
+    {
+      category: SemanticCategory.DEFINITION,
+      entity: SemanticEntity.ENUM,
+      modifiers: () => ({ is_generic: true }),
+    },
+  ],
+  [
+    "def.enum_variant",
+    {
+      category: SemanticCategory.DEFINITION,
+      entity: SemanticEntity.ENUM_MEMBER,
     },
   ],
   [
@@ -101,10 +124,50 @@ export const RUST_CORE_MAPPINGS = new Map<string, CaptureMapping>([
     },
   ],
   [
+    "def.function.generic",
+    {
+      category: SemanticCategory.DEFINITION,
+      entity: SemanticEntity.FUNCTION,
+      modifiers: () => ({ is_generic: true }),
+    },
+  ],
+  [
+    "def.function.async",
+    {
+      category: SemanticCategory.DEFINITION,
+      entity: SemanticEntity.FUNCTION,
+      modifiers: () => ({ is_async: true }),
+    },
+  ],
+  [
+    "def.function.const",
+    {
+      category: SemanticCategory.DEFINITION,
+      entity: SemanticEntity.FUNCTION,
+      modifiers: () => ({ is_const: true }),
+    },
+  ],
+  [
+    "def.function.unsafe",
+    {
+      category: SemanticCategory.DEFINITION,
+      entity: SemanticEntity.FUNCTION,
+      modifiers: () => ({ is_unsafe: true }),
+    },
+  ],
+  [
     "def.method",
     {
       category: SemanticCategory.DEFINITION,
       entity: SemanticEntity.METHOD,
+    },
+  ],
+  [
+    "def.method.associated",
+    {
+      category: SemanticCategory.DEFINITION,
+      entity: SemanticEntity.METHOD,
+      modifiers: () => ({ is_static: true }),
     },
   ],
   [
@@ -147,6 +210,172 @@ export const RUST_CORE_MAPPINGS = new Map<string, CaptureMapping>([
     {
       category: SemanticCategory.DEFINITION,
       entity: SemanticEntity.TYPE,
+    },
+  ],
+
+  // Const items (different from def.constant)
+  [
+    "def.const",
+    {
+      category: SemanticCategory.DEFINITION,
+      entity: SemanticEntity.CONSTANT,
+    },
+  ],
+
+  // Static items
+  [
+    "def.static",
+    {
+      category: SemanticCategory.DEFINITION,
+      entity: SemanticEntity.VARIABLE,
+      modifiers: () => ({ is_static: true }),
+    },
+  ],
+
+  // Parameters (different from def.parameter)
+  [
+    "def.param",
+    {
+      category: SemanticCategory.DEFINITION,
+      entity: SemanticEntity.PARAMETER,
+    },
+  ],
+
+  // Loop variables
+  [
+    "def.loop_var",
+    {
+      category: SemanticCategory.DEFINITION,
+      entity: SemanticEntity.VARIABLE,
+      modifiers: () => ({ is_loop_var: true }),
+    },
+  ],
+
+  // Module definitions
+  [
+    "def.module",
+    {
+      category: SemanticCategory.DEFINITION,
+      entity: SemanticEntity.MODULE,
+    },
+  ],
+
+  // Interface definitions (different from def.trait)
+  [
+    "def.interface",
+    {
+      category: SemanticCategory.DEFINITION,
+      entity: SemanticEntity.INTERFACE,
+    },
+  ],
+  [
+    "def.interface.generic",
+    {
+      category: SemanticCategory.DEFINITION,
+      entity: SemanticEntity.INTERFACE,
+      modifiers: () => ({ is_generic: true }),
+    },
+  ],
+
+  // Type alias definitions (different from def.type)
+  [
+    "def.type_alias",
+    {
+      category: SemanticCategory.DEFINITION,
+      entity: SemanticEntity.TYPE_ALIAS,
+    },
+  ],
+
+  // Type parameters
+  [
+    "def.type_param",
+    {
+      category: SemanticCategory.DEFINITION,
+      entity: SemanticEntity.TYPE_PARAMETER,
+    },
+  ],
+
+  // Const parameters
+  [
+    "def.const_param",
+    {
+      category: SemanticCategory.DEFINITION,
+      entity: SemanticEntity.PARAMETER,
+      modifiers: () => ({ is_const: true }),
+    },
+  ],
+
+  // Constructor functions
+  [
+    "def.constructor",
+    {
+      category: SemanticCategory.DEFINITION,
+      entity: SemanticEntity.CONSTRUCTOR,
+    },
+  ],
+
+  // Trait methods
+  [
+    "def.trait_method",
+    {
+      category: SemanticCategory.DEFINITION,
+      entity: SemanticEntity.METHOD,
+      modifiers: () => ({ is_trait_method: true }),
+    },
+  ],
+  [
+    "def.trait_method.default",
+    {
+      category: SemanticCategory.DEFINITION,
+      entity: SemanticEntity.METHOD,
+      modifiers: () => ({ is_trait_method: true, has_default_impl: true }),
+    },
+  ],
+
+  // Trait implementation methods
+  [
+    "def.trait_impl_method",
+    {
+      category: SemanticCategory.DEFINITION,
+      entity: SemanticEntity.METHOD,
+      modifiers: () => ({ is_trait_impl_method: true }),
+    },
+  ],
+  [
+    "def.trait_impl_method.async",
+    {
+      category: SemanticCategory.DEFINITION,
+      entity: SemanticEntity.METHOD,
+      modifiers: () => ({ is_trait_impl_method: true, is_async: true }),
+    },
+  ],
+
+  // Associated types
+  [
+    "def.associated_type",
+    {
+      category: SemanticCategory.DEFINITION,
+      entity: SemanticEntity.TYPE_ALIAS,
+      modifiers: () => ({ is_associated: true }),
+    },
+  ],
+
+  // Associated constants
+  [
+    "def.associated_const",
+    {
+      category: SemanticCategory.DEFINITION,
+      entity: SemanticEntity.CONSTANT,
+      modifiers: () => ({ is_associated: true }),
+    },
+  ],
+
+  // Macro definitions
+  [
+    "def.macro",
+    {
+      category: SemanticCategory.DEFINITION,
+      entity: SemanticEntity.MACRO,
     },
   ],
 
@@ -261,6 +490,38 @@ export const RUST_CORE_MAPPINGS = new Map<string, CaptureMapping>([
     {
       category: SemanticCategory.REFERENCE,
       entity: SemanticEntity.INTERFACE,
+    },
+  ],
+
+  // Macro references
+  [
+    "ref.macro",
+    {
+      category: SemanticCategory.REFERENCE,
+      entity: SemanticEntity.MACRO,
+    },
+  ],
+  [
+    "ref.macro.scoped",
+    {
+      category: SemanticCategory.REFERENCE,
+      entity: SemanticEntity.MACRO,
+    },
+  ],
+  [
+    "ref.macro.builtin",
+    {
+      category: SemanticCategory.REFERENCE,
+      entity: SemanticEntity.MACRO,
+      modifiers: () => ({ is_builtin: true }),
+    },
+  ],
+  [
+    "ref.macro.async",
+    {
+      category: SemanticCategory.REFERENCE,
+      entity: SemanticEntity.MACRO,
+      modifiers: () => ({ is_async_macro: true }),
     },
   ],
 
